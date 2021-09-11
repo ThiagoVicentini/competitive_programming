1e8ac8756e83044194e5527e49655106f67d3433
Thiago Henrique Vicentini <thiago-650@usp.br>
2021-09-10 23:30:18 -0300

    Adding ex B from week 3

--- SCC0211 - Laboratório de Algoritmos Avançados II/Semana 3 - Backtracking/exB.cpp
+++ SCC0211 - Laboratório de Algoritmos Avançados II/Semana 3 - Backtracking/exB.cpp
@@ -2,40 +2,73 @@
 
 using namespace std;
 
-vector<int> squares;
+#define sizeBoard 10
+
+vector<pair<int,int>> positions;
+char board[sizeBoard][sizeBoard];
+int maxMov = 0;
+
+int movesBlack [4][2] = {   { 1, 1}, 
+                            {-1,-1},
+                            { 1,-1},
+                            {-1, 1} };
+
+int movesWhite [4][2] = {   { 2, 2}, 
+                            {-2,-2},
+                            { 2,-2},
+                            {-2, 2} };
+
+bool isPossibleMove(int x, int y, int i){
+    if( (x+movesWhite[i][0] >= 0) and (x+movesWhite[i][0] < sizeBoard) )
+        if( (y+movesWhite[i][1] >= 0) and (y+movesWhite[i][1] < sizeBoard) )
+            if( board[x+movesBlack[i][0]][y+movesBlack[i][1]] == 'B' )
+                if( board[x+movesWhite[i][0]][y+movesWhite[i][1]] == '#' )
+                    return true;
+    return false;
+}
+
+void backtrack(int x, int y, int mov){
+    if(mov > maxMov)
+        maxMov = mov;
+    for(int i=0; i<4; i++){ // 4 possibles moviments (diagonals)            
+        if(isPossibleMove(x, y, i)){
+            board[x][y] = '#';
+            board[x+movesBlack[i][0]][y+movesBlack[i][1]] = '#';
+            board[x+movesWhite[i][0]][y+movesWhite[i][1]] = 'W';
+            backtrack(x+movesWhite[i][0], y+movesWhite[i][1], mov+1);
+            board[x][y] = 'W';
+            board[x+movesBlack[i][0]][y+movesBlack[i][1]] = 'B';
+            board[x+movesWhite[i][0]][y+movesWhite[i][1]] = '#';
+        }
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(NULL);
 
-    int numbeOfSquares;
-    cin >> numbeOfSquares;
+    int numbeOfTestCases;
+    cin >> numbeOfTestCases;
 
-    for (int i = 0; i < numbeOfSquares; i++) {
-        int height;
-        cin >> height;
-
-        if(numbeOfSquares==1){
-            cout << "Yes" << endl;
-            continue;
+    for (int i = 0; i<numbeOfTestCases; i++){
+        // Input
+        for(int j=0; j<sizeBoard; j++){
+            for(int k=0; k<sizeBoard; k++){
+                cin >> board[j][k];
+                if(board[j][k] == 'W')
+                    positions.push_back(make_pair(j, k));
+            }   
         }
 
-        if(i==0){
-            squares.push_back(height);
-            continue;
+        // Backtracking
+        for(int k=0; k<(int)positions.size(); k++){
+            backtrack(positions[k].first, positions[k].second, 0);
         }
+        
+        // Output
+        cout << maxMov << endl;
 
-        if(height > squares[i-1]){
-            height--;
-            squares.push_back(height);
-        }else if(height == squares[i-1]){
-            squares.push_back(height);
-        }else{
-            cout << "No" << endl;
-            break;
-        }
-
-        if(i==numbeOfSquares-1)
-            cout << "Yes" << endl;
+        positions.clear();
+        maxMov=0;
     }
 
     return 0;
